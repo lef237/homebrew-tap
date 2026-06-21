26b1470c70656f3cbc1729d98d52528969e915ae
lef237 <lef237k5@gmail.com>
2026-06-21 17:10:04 +0900

    gst 0.5.0

--- Formula/gst.rb
+++ Formula/gst.rb
@@ -1,8 +1,8 @@
 class Gst < Formula
   desc "Read-only Git status visualizer for the terminal"
   homepage "https://github.com/lef237/gst"
-  url "https://github.com/lef237/gst/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "19256f3e53e8ae822878d2412b694d87062e6fcf112c69106598581275f685a9"
+  url "https://github.com/lef237/gst/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "0fec86e57b3a6540de7ffe40c5b71988de71ae4332428bfdb2c43d600c9423c1"
   license "MIT"
   head "https://github.com/lef237/gst.git", branch: "main"
 
